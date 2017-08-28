c3be2c1b8943eff5d501ca97fe4d2eae1e66e432
williamlfang <william.lian.fang@gmail.com>
2017-08-28 11:13:11 +0800

    增加 Ubuntu 安装新系统的必备软件

---
+++ Ubuntu/install.sh
@@ -0,0 +1,30 @@
+sudo apt install terminator
+
+sudo apt install sshpass
+
+sudo apt install synergy
+
+sudo apt install docky
+
+wget http://cdn2.ime.sogou.com/dl/index/1491565850/sogoupinyin_2.1.0.0086_amd64.deb?st=qJVLOVGlpnTDw9b3tCvbjg&e=1503818943&fn=sogoupinyin_2.1.0.0086_amd64.deb
+
+sudo dpkg -i sougou.deb
+
+sudo apt install -f
+
+>> Input Method
+
+sudo apt install fcitx-config-gtk
+
+>> fcitx-Configuration
+>> Language Support
+
+sudo apt remove fonts-arphic-ukai fonts-arphic-uming
+
+sudo apt purge sogoupinyin
+
+
+sudo apt install r-base r-base-dev
+
+sudo dpkg -i rstudio.deb
+
